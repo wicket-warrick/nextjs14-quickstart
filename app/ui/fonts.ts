57cc701256ae142f649e0be72b23f0c5a247b9b7
hugo.suarez <hugo.suarez@altia.es>
2023-11-28 09:58:24 +0100

    Lesson 6
    Init Db config

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+})
